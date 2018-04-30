1cffe6dcc1faf589342301e90a42cff5b75b9c12
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-04-30 19:50:42 +0900

    ユニークキーを追加

--- db/migrate/20180427043149_devise_create_users.rb
+++ db/migrate/20180427043149_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       ## Database authenticatable
       t.string :name,               null: false, unique: true
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
