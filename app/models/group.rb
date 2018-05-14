38e63b5ed98ff95a09d8eb9d5f603a0a0b17f23a
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-05-14 15:16:34 +0900

    メソッドが連続しないようにgroupモデルで定義して一つにまとめた

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,8 +9,15 @@
 #
 
 class Group < ApplicationRecord
+  def error_messages_count
+    self.error_messages.size
+  end
+
+  def error_messages
+    self.errors.full_messages
+  end
+
   has_many :group_users
   has_many :users, through: :group_users
-
   validates :name, uniqueness: true, presence: true
 end
