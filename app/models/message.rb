aa8b9505c6c5f695ac5b6c8005c8ab08763c34ed
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-05-16 09:17:09 +0900

    messageモデルにスキーマを記述

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :bigint(8)        not null, primary key
+#  body       :string(255)
+#  image      :string(255)
+#  group_id   :bigint(8)
+#  user_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
