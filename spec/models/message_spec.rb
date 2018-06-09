95e0e500b0924cc0ce2718325df308cbd6194211
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-06-09 17:18:55 +0900

    group_idがないと保存できない場合を記述

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -22,6 +22,12 @@ RSpec.describe Message, type: :model do
         message.valid?
         expect(message.errors[:body_or_image]).to include('を入力してください')
       end
+
+       it 'is invalid without group_id' do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group]).to include('を入力してください')
+      end
     end
   end
 end
