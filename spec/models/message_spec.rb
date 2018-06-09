61e54e5b3b36c431d56d948c01764d4a6f6923f1
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-06-09 17:29:25 +0900

    ミスを修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   describe '#create' do
-    body 'can save' do
+    context 'can save' do
 
       it 'is valid with body' do
         expect(build(:message, image:  nil)).to be_valid
@@ -16,7 +16,7 @@ RSpec.describe Message, type: :model do
       end
     end
 
-    body 'can not save' do
+    context 'can not save' do
       it 'is invalid without body and image' do
         message = build(:message, body: nil, image: nil)
         message.valid?
