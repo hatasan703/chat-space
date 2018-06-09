04b48e8419b01629ced5da948c199edc6daf187d
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-06-09 17:01:01 +0900

    メッセージがある場合に保存できる場合を記述

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   describe '#create' do
     body 'can save' do
+      it 'is valid with body' do
+        expect(build(:message, image:  nil)).to be_valid
+      end
     end
 
     body 'can not save' do
