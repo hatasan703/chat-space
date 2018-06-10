d3185d6b4ce6ce2f790b808039f2ca4db731bcd8
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-06-10 17:43:04 +0900

    ログインしていない場合に意図したビューにリダイレクトできているかをテスト

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -23,13 +23,16 @@ describe MessagesController do
       it "renders index" do
         expect(response).to render_template :index
       end
-
     end
 
     context "not log in" do
       before do
         get :index, params: { group_id: group.id }
       end
-    end
 
+      it "redirects to new_user_session_path" do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
   end
+end
