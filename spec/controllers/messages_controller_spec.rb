1920e24877b1d2a47441d72b086f99fb7cd7b4fd
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-06-11 12:50:10 +0900

    ログインしているかつ保存に失敗した場合、メッセージの保存が行われなかったかどうかをテスト

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -61,6 +61,16 @@ describe MessagesController do
       end
 
       context "can not save" do
+        let(:invaild_params) { { group_id: group.id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invaild_params
+        }
+
+        it "does not count up" do
+          expect{ subject }.not_to change(Message, :count)
+        end
       end
     end
 
