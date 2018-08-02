3aabbb3ad711b917989b4b64f409b0ca1aa48a28
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-08-02 17:58:13 +0900

    ユーザー検索の際に検索結果が消えるように修正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,9 +1,9 @@
 $(function() {
   function appendSearchUser(user){
-    var html = `<div class="chat-group-user clearfix" id='chat-group-user'>
+    var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
-                   <a class="user_search_add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
-                   </a>
+                  <a class="user_search_add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
+                  </a>
                 </div>`
     $('#user-search-result').append(html);
   }
@@ -14,35 +14,32 @@ $(function() {
                     <input name='chat_group[user_ids][]' type='hidden' value='${id}'>
                     <p class='chat-group-user__name'>${name}</p>
                     <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn data-user-id="${id}" data-user-name="${name}"'>削除</a>
-                  </div>
+                    </div>
                 </div>`
     $("#chat-group-users").append(html)
   }
   $(function() {
-  $("#user-search-field").on("keyup", function(e) {
-    e.preventDefault()
-    var input = $("#user-search-field").val();
-    if(input !== ""){
-      $.ajax({
-        type: 'GET',
-        url: '/users',
-        data: { keyword: input },
-        dataType: 'json'
-      })
+    $("#user-search-field").on("keyup", function() {
+      var input = $("#user-search-field").val();
+        $.ajax({
+          type: 'GET',
+          url: '/users',
+          data: { keyword: input },
+          dataType: 'json'
+        })
+
       .done(function(users){
         $("#user-search-result").empty();
+        if (users.lengt !== 0) {
           users.forEach(function(user){
             appendSearchUser(user);
           })
+        }
       })
       .fail(function(){
-        alert('error');
+        alert('ユーザー検索に失敗しました');
       });
-    }
-    else{
-      $('#user-search-result').remove();
-    };
-  });
+    });
 });
   $(function() {
     $(document).on("click", '.user_search_add', function() {
