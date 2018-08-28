92def0ae35c835a92b3d995b60f967d5384f2e00
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-08-28 16:35:54 +0900

    fix to fetch env var

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,21 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
+  if Rails.env.production?
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+  }
+  else
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
+  end
 
   config.fog_directory  = 'hata-chat-space'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hata-chat-space'
