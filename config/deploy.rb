fb66bfa676e79eabde2a9b57c5794836d075b06b
hiroki-hatakeda <h.hatasan09@gmail.com>
2018-08-29 20:12:15 +0900

    add sym link to secret.yml on prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,8 @@ set :application, 'chat-space'
 set :repo_url,  'git@github.com:hatasan703/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{ config/secrets.yml }
+
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.4.0'
